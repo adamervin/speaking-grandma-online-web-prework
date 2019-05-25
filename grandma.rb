3bc6fa9ed03336e1193327beb8826cc6d4d6aa1e
Adam Ervin <adamjervin@gmail.com>
2019-05-25 21:47:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,14 @@
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase) #checks for uppcase?
   return "HUH?! SPEAK UP, SONNY!"
   
-if (phrase)
+if (phrase)  #upcase??
   return "NO, NOT SINCE 1938!"
+  
+else 
+  return 
+  
+  
+  
   
 
   
